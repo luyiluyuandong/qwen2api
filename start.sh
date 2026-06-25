e1a788ddce34efcecc8c68cc2c2e9c7b41ecd0d2
luyiluyuandong <373974335@qq.com>
2026-06-25 12:38:53 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
 set -e
 
-if [ -z "$PORT" ]; then
-  echo "PORT is not set"
-  exit 1
-fi
+CONFIG_TEMPLATE="/etc/sing-box/config.template.json"
+CONFIG_FILE="/etc/sing-box/config.json"
 
-if [ -z "$PROXY_USER" ]; then
-  echo "PROXY_USER is not set"
-  exit 1
-fi
+for var in PORT PROXY_USER PROXY_PASS; do
+  eval "value=\$$var"
+  if [ -z "$value" ]; then
+    echo "FATAL: $var is not set"
+    exit 1
+  fi
+done
 
-if [ -z "$PROXY_PASS" ]; then
-  echo "PROXY_PASS is not set"
+if [ ! -f "$CONFIG_TEMPLATE" ]; then
+  echo "FATAL: Template $CONFIG_TEMPLATE not found"
   exit 1
 fi
 
 sed \
-  -e "s/\${PORT}/$PORT/g" \
-  -e "s/\${PROXY_USER}/$PROXY_USER/g" \
-  -e "s/\${PROXY_PASS}/$PROXY_PASS/g" \
-  /etc/sing-box/config.template.json > /etc/sing-box/config.json
+  -e "s|\${PORT}|$PORT|g" \
+  -e "s|\${PROXY_USER}|$PROXY_USER|g" \
+  -e "s|\${PROXY_PASS}|$PROXY_PASS|g" \
+  "$CONFIG_TEMPLATE" > "$CONFIG_FILE"
 
 echo "Starting sing-box on port $PORT"
 
-sing-box run -c /etc/sing-box/config.json
+trap 'kill $PID; wait $PID; exit 0' TERM INT
+
+sing-box run -c "$CONFIG_FILE" &
+PID=$!
+wait $PID
