cde79405ad391b06da881375e48969287428dd4c
luyiluyuandong <373974335@qq.com>
2026-06-25 13:00:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 CONFIG_TEMPLATE="/etc/sing-box/config.template.json"
 CONFIG_FILE="/etc/sing-box/config.json"
 
-for var in PORT PROXY_USER PROXY_PASS; do
+for var in PORT UUID; do
   eval "value=\$$var"
   if [ -z "$value" ]; then
     echo "FATAL: $var is not set"
@@ -19,11 +19,10 @@ fi
 
 sed \
   -e "s|\${PORT}|$PORT|g" \
-  -e "s|\${PROXY_USER}|$PROXY_USER|g" \
-  -e "s|\${PROXY_PASS}|$PROXY_PASS|g" \
+  -e "s|\${UUID}|$UUID|g" \
   "$CONFIG_TEMPLATE" > "$CONFIG_FILE"
 
-echo "Starting sing-box on port $PORT"
+echo "Starting sing-box (VLESS+WS) on port $PORT"
 
 trap 'kill $PID; wait $PID; exit 0' TERM INT
 
